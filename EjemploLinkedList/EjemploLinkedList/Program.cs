2100412def79bb69d45877cdc204f038a7194bb1
Daniela García <DanyGa02@hotmail.com>
2020-12-21 09:11:02 -0600

    sorry

---
+++ EjemploLinkedList/EjemploLinkedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notas = new LinkedList<string>();
+            notas.AddFirst("do");
+            notas.AddLast("so");
+
+            notas.AddAfter(notas.First, "re");
+            notas.AddAfter(notas.First.Next, "mi");
+            notas.AddBefore(notas.Last, "fa");
+
+            // Contenido: do re mi fa so
+
+            notas.RemoveFirst();
+            notas.RemoveLast();
+
+            // Contenido: re mi fa
+
+            LinkedListNode<string> nota = notas.Find("mi");
+            notas.Remove(nota);
+            notas.AddFirst(nota);
+
+            // Contenido: mi re fa
+
+            foreach (string n in notas)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
